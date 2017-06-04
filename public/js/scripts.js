a42d091b2bb0e1c1f6d12f2e151f3888105c496a
Masahiro Harada <poe.422@gmail.com>
2017-06-04 23:44:58 +0900

    Add solution codes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,36 @@
 $(function() {
 
+    var currentPage = 1;
+
+    $('.load-more').click(function () {
+        currentPage += 1;
+        $.ajax({
+            method: 'get',
+            url: '/api/gif?page=' + currentPage,
+            success: function(response) {
+                // アイテム追加
+                var items = '';
+                response.GifList.forEach(function (item) {
+                    items += `<div class="gif-item">
+                        <div class="content">
+                            <div class="image-box">
+                                <a href="/public/img/${item.Filename}" data-lightbox="${item.Filename}">
+                                    <img src="/public/img/${item.Filename}">
+                                </a>
+                            </div>
+                        </div>
+                    </div>`;
+                });
+                $('.gif').append($(items));
+
+                // 最後のページだった場合はもっと見るボタンを隠す
+                if (response.IsLastPage) {
+                    $('.load-more').hide();
+                }
+            }
+        });
+    });
+
     /**
      * Lightbox
      * http://lokeshdhakar.com/projects/lightbox2/
